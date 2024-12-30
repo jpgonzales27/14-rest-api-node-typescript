d886917b2ba90eeb9aad64d5506eaf1e3114c8ff
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-29 20:40:20 -0400

    usando express para una peticion GET

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { sumar } from "./server";
+import server from "./server";
 
-console.log("desde index.ts");
-
-sumar();
-
-console.log("despues de sumar");
+server.listen(4000, () => {
+  console.log(`REST API en el puerto 4000`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
-export function sumar() {
-  console.log(2 + 2);
-}
+import express from "express";
+
+const server = express();
+
+//Routing
+server.get("/", (req, res) => {
+  const datos = [
+    { id: 1, nombre: "Juan" },
+    { id: 2, nombre: "Pablo" },
+  ];
+  res.json(datos);
+});
+
+export default server;
