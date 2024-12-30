e2c531d1d634fd9d9400ee5cf4f5f0fa20c2dc16
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-12-29 22:07:46 -0400

    instalando sequelize - pg - dotenv para conectarse a la BD

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import router from "./router";
+import db from "./config/db";
+
+// Conectar a base de datos
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log("Conexión exitosa a la BD");
+  } catch (error) {
+    console.log(error);
+    console.log("Hubo un error al conectar a la BD");
+  }
+}
+connectDB();
 
 const server = express();
-
 //Routing
 server.use("/api/products", router);
 
