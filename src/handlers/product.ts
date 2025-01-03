59ceff59e0f0caf7f7b03bb26c3a3fb47b2580e4
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-03 10:44:50 -0400

    usando express validator para proteger los campos del body

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.models";
+import { check, validationResult } from "express-validator";
 
-export const createProduct = async (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
   console.log(req.body);
+
+  // Validación
+  await check("name").notEmpty().withMessage("El nombre de Producto no puede ir vacio").run(req);
+  await check("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio de Producto no puede ir vacio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido")
+    .run(req);
+
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+
   const product = await Product.create(req.body);
   res.json({ data: product });
 };
