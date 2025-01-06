e9f34fe256ab313db0c32153b24eacb5bb65cdd1
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-06 10:37:28 -0400

    creando test para validar el POST de products

---
+++ src/handlers/__tests__/product.test.ts
@@ -0,0 +1,53 @@
+import request from "supertest";
+import server from "../../server";
+
+describe("POST /api/products", () => {
+  it("should display validation errors", async () => {
+    const response = await request(server).post("/api/products").send();
+    expect(response.status).toEqual(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(4);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(2);
+  });
+  it("should validate that the price is greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Monitor Curvo",
+      price: 0,
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].msg).toEqual("Precio no válido");
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(2);
+  });
+
+  it("should validate that the price is a number and greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Monitor Curvo",
+      price: "Hola",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(2);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(4);
+  });
+  it("should create a new product", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Mouse - Testing",
+      price: 50,
+    });
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("data");
+
+    expect(response.status).not.toBe(404);
+    expect(response.status).not.toBe(200);
+    expect(response.body).not.toHaveProperty("errors");
+  });
+});
