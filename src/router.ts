11c5d5b6732cd2e1db02a587d01beccf547477a8
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-06 14:37:24 -0400

    Documentando el metodo POST

--- src/router.ts
+++ src/router.ts
@@ -89,6 +89,34 @@ router.get("/", getProducts);
  */
 router.get("/:id", param("id").isInt().withMessage("ID no válido"), handleInputErrors, getProductById);
 
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *      summary: Creates a new product
+ *      tags:
+ *          - Products
+ *      description: Returns a new record in the database
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: "Monitor Curvo 49 Pulgadas"
+ *                          price:
+ *                              type: number
+ *                              example: 399
+ *      responses:
+ *          201:
+ *              description: Product created successfully
+ *          400:
+ *              description: Bad Request - invalid input data
+ *
+ */
 router.post(
   "/",
   // Validación
