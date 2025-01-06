464995bbd75515a7f74f02d42cbdd5bba8d7eab7
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-06 14:43:45 -0400

    documentando el metodo PUT

--- src/router.ts
+++ src/router.ts
@@ -112,7 +112,11 @@ router.get("/:id", param("id").isInt().withMessage("ID no válido"), handleInput
  *                              example: 399
  *      responses:
  *          201:
- *              description: Product created successfully
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
  *          400:
  *              description: Bad Request - invalid input data
  *
@@ -132,6 +136,49 @@ router.post(
   createProduct
 );
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  put:
+ *      summary: Updates a product with user input
+ *      tags:
+ *          - Products
+ *      description: Returns the updated product
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: "Monitor Curvo 49 Pulgadas"
+ *                          price:
+ *                              type: number
+ *                              example: 399
+ *                          availability:
+ *                              type: boolean
+ *                              example: true
+ *      responses:
+ *          200:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad Request - Invalid ID or Invalid input data
+ *          404:
+ *              description: Product Not Found
+ */
 router.put(
   "/:id",
   param("id").isInt().withMessage("ID no válido"),
