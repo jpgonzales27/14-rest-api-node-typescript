c0958c1bc58c21ac0a91b4fc7e8a88476f4143c8
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-06 15:22:37 -0400

    cambiando el logo y el header de swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,4 +1,5 @@
 import swaggerJSDoc from "swagger-jsdoc";
+import { SwaggerUiOptions } from "swagger-ui-express";
 
 const options: swaggerJSDoc.Options = {
   swaggerDefinition: {
@@ -19,4 +20,19 @@ const options: swaggerJSDoc.Options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerUiOptions: SwaggerUiOptions = {
+  customCss: `
+      .topbar-wrapper .link {
+          content: url('https://static.vecteezy.com/system/resources/previews/023/654/784/non_2x/golden-logo-template-free-png.png');
+          height: 80px;
+          width: auto;
+      }
+      .swagger-ui .topbar {
+          background-color: #2b3b45;
+      }
+  `,
+  customSiteTitle: "Documentación REST API Express / TypeScript",
+};
 export default swaggerSpec;
+export { swaggerUiOptions };
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import router from "./router";
 import db from "./config/db";
 import colors from "colors";
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./config/swagger";
+import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 
 // Conectar a base de datos
 export async function connectDB() {
@@ -28,6 +28,6 @@ server.use(express.json());
 server.use("/api/products", router);
 
 // Docs
-server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions));
 
 export default server;
